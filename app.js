6c142929d3c6f6d21dcb3106b9d0634c3aae2066
joseph Evangelista <oootep123@gmail.com>
2020-07-20 23:53:40 -0400

    added first variables in js

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 // api example of champion square assets
 // http://ddragon.leagueoflegends.com/cdn/10.14.1/img/champion/Aatrox.png
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const squares = document.querySelector(".grid div");
+    const result = document.querySelector("#result");
+    const displayCurrentPlayer = document.querySelector("#current-player");
+
+    squares.forEach(square => {
+        square[i].onclick = function() {
+            if 
+        }
+    });
+})
