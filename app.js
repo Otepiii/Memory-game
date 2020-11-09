0cc355d283b902aba07d539f5abba88443742161
joseph Evangelista <oootep123@gmail.com>
2020-11-08 23:22:40 -0500

    implemented styling, html and initial logic

--- app.js
+++ app.js
@@ -2,16 +2,36 @@
 // http://ddragon.leagueoflegends.com/cdn/10.14.1/img/champion/Aatrox.png
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    const squares = document.querySelector(".grid div");
-    const result = document.querySelector("#result");
-    const displayCurrentPlayer = document.querySelector("#current-player");
+const cards = document.querySelectorAll(".memory-card");
 
-    squares.forEach(square => {
-        square[i].onclick = function() {
-            if (){
-                
-            }
-        }
-    });
-})
+let hasFlippedCard = false;
+let firstCard, secondCard
+
+function flipCard(){
+    this.classList.add("flip");
+
+    if (!hasFlippedCard) {
+        // first click
+        hasFlippedCard= true;
+        firstCard = this;    
+    } else {
+        // second click
+        hasFlippedCard = false;
+        secondCard=this;
+
+        // do cards match?
+        if (firstCard.dataset.framework === secondCard.dataset.framework)  {
+            
+            firstCard.removeEventListener("click", flipCard);
+            secondCard.removeEventListener("click", flipCard)
+        } else {
+            setTimeout(() => {
+// not a match
+firstCard.classList.remove("flip");
+secondCard.classList.remove("flip");
+}, 1500)
+            }   
+    }
+}
+
+cards.forEach(card => card.addEventListener("click", flipCard))
