41e7a2721c5c625c00c1f6af52f8a9796707fafa
joseph Evangelista <oootep123@gmail.com>
2020-12-08 08:04:48 -0500

    tryung to use some code from previous commit

--- app.js
+++ app.js
@@ -1,35 +1,139 @@
-// var audio = new Audio();
-// audio.src = "assets/File0120.mp3";
-// var game = new Audio();
-// game.src = "assets/10CrystalScar.mp3"
-// var clickysound = new Audio();
-// clickysound.src = "assets/075_item_sightward_lux_obd_01.mp3"
-// var finish = new Audio();
-// finish.src = "assets/File0114.mp3"
-// var match = new Audio();
-// match.src = "assets/034_item_leviathan_buff_1.wav"
-let start = document.querySelector(".overlay-text")
+// let start = document.querySelector(".overlay-text")
 
-// var context = new (window.AudioContext || window.webkitAudioContext)();
+
+// start.addEventListener("click", () => {
+//     start.classList.remove("visible")
+//     var sound = new Pizzicato.Sound({ 
+//         source: 'file',
+//         options: { path: ['assets/File0120.mp3' ] }
+//     }, function() {
+//         console.log('sound file loaded!');
+//         sound.play()
+//     });
+//     var game = new Pizzicato.Sound({ 
+//         source: 'file',
+//         options: { path: ['assets/10CrystalScar.mp3' ] }
+//     }, function() {
+//         console.log('game sound file loaded!');
+//         game.play()
+//     });
+    // sound.play();
+    // game.play();
+// })
+
+// const cards = document.querySelectorAll(".memory-card");
+
+// let hasFlippedCard = false;
+// let lockboard = false; // variable to lock the cards if the clicked cards are not a match and renders the board not clickable for the time being
+// let firstCard, secondCard
+
+
+// function flipCard(){
+
+
+//     if (lockboard) return; //if lockboard is true this will return and not execute the rest of the function
+//     if ( this === firstCard) return; //this checks if the same card is clicked twice then it will return from the function
+
+    // this.classList.add("flip");
+
+    // if (!hasFlippedCard) {
+    //     // first click
+    //     var click = new Pizzicato.Sound({ 
+    //         source: 'file',
+    //         options: { path: ['assets/075_item_sightward_lux_obd_01.mp3' ] }
+    //     }, function() {
+    //         console.log('click sound file loaded!');
+    //         click.play()
+//         });
+//         hasFlippedCard= true;
+//         firstCard = this;    
+        
+    
+//         return;
+
+//     }
+//         // second click
+//         var click = new Pizzicato.Sound({ 
+//             source: 'file',
+//             options: { path: ['assets/075_item_sightward_lux_obd_01.mp3' ] }
+//         }, function() {
+//             console.log('click sound file loaded!');
+//             click.play()
+//         });
+//         hasFlippedCard = false;
+//         secondCard=this;
+
+//         checkForMatch();
+// }
+
+// function checkForMatch() {
+//     // clickysound.play()
+//     let isMatch = firstCard.dataset.framework === 
+//         secondCard.dataset.framework;
+      
+//         isMatch ? disableCards() : unflipCards()
+// }
+
+// function disableCards() {
+//     var match = new Pizzicato.Sound({ 
+//         source: 'file',
+//         options: { path: ['assets/034_item_leviathan_buff_1.wav' ] }
+//     }, function() {
+//         console.log('matched cards sound file loaded!');
+//         match.play()
+//     });
+//     firstCard.removeEventListener("click", flipCard);
+//     secondCard.removeEventListener("click", flipCard);
+
+    
+
+//     resetBoard();
+// }
+
+// function unflipCards() {
+//     lockboard = true;
+
+//     setTimeout(() => {
+//         // not a match
+//         firstCard.classList.remove("flip");
+//         secondCard.classList.remove("flip");
+        
+//         resetBoard();
+//         }, 1600)
+// }
+
+// function resetBoard() { // this resets the board , first and second card to false and null using es6 destructuring assignment
+//     [hasFlippedCard, lockboard] = [false, false];
+//     [firstCard, secondCard] = [null, null];
+// }
+
+// (function shuffle() {  // this functions shuffles the card randomly and sets their positions by order
+//     cards.forEach(card => { // foreach function makes the function go through each card
+//         let randomPos = Math.floor(Math.random() * 12);// math.floor rounds the number to a whole number //math.random generates a random number to multipy by 12
+//         card.style.order = randomPos; // this assigns the order position of each cards
+//     });
+// })(); // by wrapping the function in a parenthesis and then writing a open and closed parenthesis right after it
+//         // and makes it a IIFE (immediately invoked function expression) which means this function will be executed right after its definition
+
+
+// cards.forEach(card => card.addEventListener("click", flipCard));
+
+var audio = new Audio();
+audio.src = "assets/File0120.mp3";
+var game = new Audio();
+game.src = "assets/10CrystalScar.mp3"
+var clickysound = new Audio();
+clickysound.src = "assets/075_item_sightward_lux_obd_01.mp3"
+var finish = new Audio();
+finish.src = "assets/File0114.mp3"
+var match = new Audio();
+match.src = "assets/034_item_leviathan_buff_1.wav"
+let start = document.querySelector(".overlay-text")
 
 start.addEventListener("click", () => {
     start.classList.remove("visible")
-    var sound = new Pizzicato.Sound({ 
-        source: 'file',
-        options: { path: ['assets/File0120.mp3' ] }
-    }, function() {
-        console.log('sound file loaded!');
-        sound.play()
-    });
-    var game = new Pizzicato.Sound({ 
-        source: 'file',
-        options: { path: ['assets/10CrystalScar.mp3' ] }
-    }, function() {
-        console.log('game sound file loaded!');
-        game.play()
-    });
-    // sound.play();
-    // game.play();
+    audio.play();
+    game.play();
 })
 
 const cards = document.querySelectorAll(".memory-card");
@@ -49,13 +153,7 @@ function flipCard(){
 
     if (!hasFlippedCard) {
         // first click
-        var click = new Pizzicato.Sound({ 
-            source: 'file',
-            options: { path: ['assets/075_item_sightward_lux_obd_01.mp3' ] }
-        }, function() {
-            console.log('click sound file loaded!');
-            click.play()
-        });
+        clickysound.play()
         hasFlippedCard= true;
         firstCard = this;    
         
@@ -64,13 +162,7 @@ function flipCard(){
 
     }
         // second click
-        var click = new Pizzicato.Sound({ 
-            source: 'file',
-            options: { path: ['assets/075_item_sightward_lux_obd_01.mp3' ] }
-        }, function() {
-            console.log('click sound file loaded!');
-            click.play()
-        });
+        clickysound.play()
         hasFlippedCard = false;
         secondCard=this;
 
@@ -86,13 +178,7 @@ function checkForMatch() {
 }
 
 function disableCards() {
-    var match = new Pizzicato.Sound({ 
-        source: 'file',
-        options: { path: ['assets/034_item_leviathan_buff_1.wav' ] }
-    }, function() {
-        console.log('matched cards sound file loaded!');
-        match.play()
-    });
+    match.play()
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
 
