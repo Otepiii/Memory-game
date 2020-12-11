ce7d98af937927d48063d77f9ff3b18aa3372542
joseph Evangelista <oootep123@gmail.com>
2020-12-11 00:02:45 -0500

    finished the game initially

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 var finish = new Audio();
 finish.src = "assets/File0114.mp3"
+var game = new Audio();
+game.src = "assets/10CrystalScar.mp3"
 let start = document.querySelector(".overlay-text")
+var restartBtn = document.querySelector("#victory-text") 
 
 
 start.addEventListener("click", () => {
     start.classList.remove("visible")
     var audio = new Audio();
-audio.src = "assets/File0120.mp3";
-var game = new Audio();
-game.src = "assets/10CrystalScar.mp3"
+    audio.src = "assets/File0120.mp3";
+    // var game = new Audio();
+    // game.src = "assets/10CrystalScar.mp3"
     audio.play();
-    game.play();
+    game.play(); 
 })
 
 const cards = document.querySelectorAll(".memory-card");
+var matchedCards = 0;
+console.log(matchedCards)
 
 let hasFlippedCard = false;
 let lockboard = false; // variable to lock the cards if the clicked cards are not a match and renders the board not clickable for the time being
 let firstCard, secondCard
+
+
 
 
 function flipCard(){
@@ -31,7 +38,7 @@ function flipCard(){
     if (!hasFlippedCard) {
         // first click
         var clickysound = new Audio();
-clickysound.src = "assets/075_item_sightward_lux_obd_01.mp3"
+        clickysound.src = "assets/075_item_sightward_lux_obd_01.mp3"
         clickysound.play()
         hasFlippedCard= true;
         firstCard = this;    
@@ -42,7 +49,7 @@ clickysound.src = "assets/075_item_sightward_lux_obd_01.mp3"
     }
         // second click
         var clickysound = new Audio();
-clickysound.src = "assets/075_item_sightward_lux_obd_01.mp3"
+        clickysound.src = "assets/075_item_sightward_lux_obd_01.mp3"
         clickysound.play()
         hasFlippedCard = false;
         secondCard=this;
@@ -56,18 +63,23 @@ function checkForMatch() {
         secondCard.dataset.framework;
       
         isMatch ? disableCards() : unflipCards()
+
 }
 
 function disableCards() {
     var match = new Audio();
-match.src = "assets/034_item_leviathan_buff_1.wav"
+    match.src = "assets/034_item_leviathan_buff_1.wav"
     match.play()
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
-
+    matchedCards++;
+    matchedCards++;
     
-
     resetBoard();
+
+    if(matchedCards === cards.length){
+        victory()
+    }
 }
 
 function unflipCards() {
@@ -87,6 +99,19 @@ function resetBoard() { // this resets the board , first and second card to fals
     [firstCard, secondCard] = [null, null];
 }
 
+function victory(){
+    finish.play();
+    game.pause();
+    document.getElementById("victory-text").classList.add("visible")
+    hideCards();
+}
+
+function hideCards() {
+    cards.forEach(card => {
+        card.classList.remove("flip");
+    })
+}
+
 (function shuffle() {  // this functions shuffles the card randomly and sets their positions by order
     cards.forEach(card => { // foreach function makes the function go through each card
         let randomPos = Math.floor(Math.random() * 12);// math.floor rounds the number to a whole number //math.random generates a random number to multipy by 12
@@ -95,5 +120,9 @@ function resetBoard() { // this resets the board , first and second card to fals
 })(); // by wrapping the function in a parenthesis and then writing a open and closed parenthesis right after it
         // and makes it a IIFE (immediately invoked function expression) which means this function will be executed right after its definition
 
+
+restartBtn.addEventListener("click", () => {
+    window.location.reload();
+})
 
 cards.forEach(card => card.addEventListener("click", flipCard));
